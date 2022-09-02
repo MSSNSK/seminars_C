4954bbd0b6d19fb76fd5c35162141b6d43f08f60
Markov Sergei <irbis_mssw@mail.ru>
2022-09-02 16:44:48 +0700

    Fixed task29

--- seminar_4/task29/Program.cs
+++ seminar_4/task29/Program.cs
@@ -3,24 +3,62 @@
 // 5, 0, 20 -> [1, 2, 5, 7, 19]
 // 3, 1, 35 -> [6, 1, 33]
 
-int [] Array()
-{   
-    Console.WriteLine("Enter the number of array elements: ");
-    int num = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter the initial element of the array: ");
-    int init = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter the final element of the array: ");
-    int final = int.Parse(Console.ReadLine());
 
-    int [] arr = new int [num];
+int [] Array(int number, int initial, int fin)
+{   
+    int [] arr = new int [number];
     for(int i = 0; i < arr.Length; i++)
     {
-        arr [i] = new Random().Next(init, final);
+        arr [i] = new Random().Next(initial, fin);
     }
     return arr;
 }
 
-Console.WriteLine(String.Join(", ", Array()));
+Console.WriteLine("Enter the number of array elements: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the initial element of the array: ");
+int init = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the final element of the array: ");
+int final = int.Parse(Console.ReadLine());
+
+int number = num;
+int initial = init;
+int fin = final;
+
+if (final < init)
+{
+    Console.WriteLine("Input error, the initial element is larger than the final one!");
+}
+else if (num == 0)
+{
+    Console.WriteLine("Input error, the array must contain more than 0 elements!");
+}
+else
+{
+    Console.WriteLine(String.Join(", ", Array(number, initial, fin)));
+}
+
+
+// РАНЕЕ СДАННЫЕ ВАРИАНТЫ
+
+// int [] Array()
+// {   
+//     Console.WriteLine("Enter the number of array elements: ");
+//     int num = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Enter the initial element of the array: ");
+//     int init = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Enter the final element of the array: ");
+//     int final = int.Parse(Console.ReadLine());
+
+//     int [] arr = new int [num];
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         arr [i] = new Random().Next(init, final);
+//     }
+//     return arr;
+// }
+
+// Console.WriteLine(String.Join(", ", Array()));
 
 
 // ВТОРОЙ ВАРИАНТ, НО В ДАННОМ СЛУЧАЕ БЛОК ВВОДА ПЕРЕМЕННЫХ ПРИШЛОСЬ ВЫНЕСТИ ПЕРЕД БЛОКОМ ФУНКЦИИ, ИНАЧЕ НЕ ОТРАБАТЫВАЛЫ ПРОВЕРКА УСЛОВИЙ НА НЕКОРРЕКТНЫЙ ВВОД. 
