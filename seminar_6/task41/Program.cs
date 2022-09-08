5063677185f3cd737b315951c4116acfdf5cd647
Markov Sergei <irbis_mssw@mail.ru>
2022-09-08 11:43:32 +0700

    Add tasks  seminar_6

---
+++ seminar_6/task41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+// -1, -7, 567, 89, 223 -> 3
+
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter {i + 1} element of an array with index {i}: ");
+        res[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return res;
+}
+
+int PositiveNumber(int[] array)
+{
+    int count = 0;
+
+    foreach (int el in array)
+    {
+        if (el > 0)
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Enter the number of array elements: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = GetArray(arraySize);
+Console.WriteLine($"The introduced array: [{String.Join(", ", newArray)}]");
+
+int result = PositiveNumber(newArray);
+Console.WriteLine($"In the array {result} positive number(s).");
+
+if (arraySize == 0) Console.WriteLine("Input error, the array must contain more than 0 elements!");
---
+++ seminar_6/task43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+// Можно было написать ещё короче, используя в качестве первичных переменных var,
+// но так так как на семинарах это не делали, использовал тип string с дальнейшим преобразованием.
+
+Console.Write("Введите k1: ");
+string num1 = Console.ReadLine();
+Console.Write("Введите b1: ");
+string num2 = Console.ReadLine();
+Console.Write("Введите k2: ");
+string num3 = Console.ReadLine();
+Console.Write("Введите b2: ");
+string num4 = Console.ReadLine();
+
+double k1 = Convert.ToDouble(num1);
+double b1 = Convert.ToDouble(num2);
+double k2 = Convert.ToDouble(num3);
+double b2 = Convert.ToDouble(num4);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+if (k1 == k2 && b1 == b2) Console.WriteLine("Directly coincide.");
+else if (k1 == k2) Console.WriteLine("The straight lines are parallel.");
+else Console.WriteLine($"The intersection point of two lines: ({x}; {y}).");
