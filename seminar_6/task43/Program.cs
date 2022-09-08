a590e1514178581aff35e39102d45f76a911b124
Markov Sergei <irbis_mssw@mail.ru>
2022-09-08 18:32:47 +0700

    Add tasks

--- seminar_6/task43/Program.cs
+++ seminar_6/task43/Program.cs
@@ -8,6 +8,7 @@
 // Можно было написать ещё короче, используя в качестве первичных переменных var,
 // но так так как на семинарах это не делали, использовал тип string с дальнейшим преобразованием.
 
+
 Console.Write("Введите k1: ");
 string num1 = Console.ReadLine();
 Console.Write("Введите b1: ");
@@ -28,3 +29,31 @@ double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
 if (k1 == k2 && b1 == b2) Console.WriteLine("Directly coincide.");
 else if (k1 == k2) Console.WriteLine("The straight lines are parallel.");
 else Console.WriteLine($"The intersection point of two lines: ({x}; {y}).");
+
+
+
+// ВАРИАНТ РЕЩЕНИЯ ЧЕРЕЗ ФУНКЦИЮ
+
+void FindIntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+    {
+        Console.Write("Прямые не пересекаются");
+    }
+    else
+    {
+        double x = (b1 - b2) / (k2 - k1);
+        double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+        Console.WriteLine($"Точка пересечения прямых ({x}; {y})");
+    }
+}
+
+Console.Write("Введите b1: ");
+double numberB1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double numberK1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double numberB2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double numberK2 = double.Parse(Console.ReadLine());
+FindIntersectionPoint(numberB1, numberK1, numberB2, numberK2);
